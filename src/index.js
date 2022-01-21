8bc3e18c89c0364f3ded7662a48bae40938c2af8
deboracaires <deboracaires@pop-os.localdomain>
2022-01-21 16:00:06 -0300

    rota post/tweets finalizada

--- src/index.js
+++ src/index.js
@@ -10,15 +10,36 @@ app.use(express.json());
 let users = [];
 let tweets = [];
 
+function findUsername(givenUsername) {
+    const user = users.filter((user) => {
+        if (user.username === givenUsername) return user;
+    })
+
+    return user;
+}
+
 app.post('/sign-up', (req, res) => {
     const user = req.body;
     if (user.username === undefined || user.avatar === undefined) {
         res.sendStatus(400);
+    } else if (findUsername(user.username).length !== 0) {
+        res.sendStatus(409);
     } else {
         users.push(user);
     }
     res.send('OK');
 })
 
+app.post('/tweets', (req, res) => {
+    const tweet = req.body;
+    if (tweet.username === undefined || tweet.tweet === undefined) {
+        res.sendStatus(400);
+    } else if (findUsername(tweet.username).length === 0) {
+        res.sendStatus(404);
+    } else {
+        tweets.push(tweet);
+    }
+    res.send('OK'); 
+})
 
 app.listen(5000);
