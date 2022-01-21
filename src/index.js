ae6b673e3398bc00ee7641effa8602c1255ce27f
deboracaires <deboracaires@pop-os.localdomain>
2022-01-21 15:47:40 -0300

    rota post/sign-up finalizada

--- src/index.js
+++ src/index.js
@@ -7,5 +7,18 @@ app.use(cors());
 
 app.use(express.json());
 
+let users = [];
+let tweets = [];
+
+app.post('/sign-up', (req, res) => {
+    const user = req.body;
+    if (user.username === undefined || user.avatar === undefined) {
+        res.sendStatus(400);
+    } else {
+        users.push(user);
+    }
+    res.send('OK');
+})
+
 
 app.listen(5000);
