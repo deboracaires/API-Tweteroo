626738e13f53ff5e6c9e86d89fdee756ba5a2482
deboracaires <deboracaires@pop-os.localdomain>
2022-01-21 17:40:33 -0300

    rota get/tweets/USERNAME finalizada

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ function findUsername(givenUsername) {
 app.post('/sign-up', (req, res) => {
     const user = req.body;
     if (user.username === undefined || user.avatar === undefined) {
-        res.sendStatus(400);
+        res.send('Todos os campos são obrigatórios!').status(400);
     } else if (findUsername(user.username).length !== 0) {
         res.sendStatus(409);
     } else {
@@ -33,7 +33,7 @@ app.post('/sign-up', (req, res) => {
 app.post('/tweets', (req, res) => {
     const tweet = req.body;
     if (tweet.username === undefined || tweet.tweet === undefined) {
-        res.sendStatus(400);
+        res.send('Todos os campos são obrigatórios!').status(400);
     } else if (findUsername(tweet.username).length === 0) {
         res.sendStatus(404);
     } else {
@@ -52,6 +52,25 @@ app.get('/tweets', (req, res) => {
         counter++;
     }
     res.send(lastTweets.slice(0,10))
+})
+
+app.get('/tweets/:username', (req, res) => {
+    const givenUsername = req.params.username;
+    const userTweets = tweets.filter((tweet) => {
+        if (givenUsername === tweet.username) return tweet;
+    })
+    if (userTweets.length === 0) res.sendStatus(404);
+    const finalUserTweets = [];
+    let counter = 0;
+    for (let i = 0; i < userTweets.length; i++) {
+        const user = findUsername(userTweets[i].username);
+        finalUserTweets.push(userTweets[i]);
+        finalUserTweets[counter].avatar = user[0].avatar;
+        counter++;
+        console.log('to aqui')
+    }
+    
+    res.send(finalUserTweets)
 })
 
 app.listen(5000);
